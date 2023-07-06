b289b744258b4097da453582e9130d2540ef371b
Alexander Filimonov <fil.dsgn@yandex.ru>
2023-07-07 02:02:11 +0300

    dev: add search form jsx

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,17 +2,22 @@
 import "./App.css";
 
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import Header from "../Header/Header.js";
 import Main from "../Main/Main.js";
-import Footer from "../Footer/Footer";
+import Movies from "../Movies/Movies.js";
+import Footer from "../Footer/Footer.js";
 
 function App() {
   return (
     <div className="App">
       <div className="page">
         <Header />
-        <Main />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/movies" element={<Movies />} />
+        </Routes>
         <Footer />
       </div>
     </div>
---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,13 @@
+import "./Movies.css";
+
+import SearchForm from "../SearchForm/SearchForm.js";
+
+function Movies() {
+  return (
+    <main className="content">
+      <SearchForm />
+    </main>
+  );
+}
+
+export default Movies;
