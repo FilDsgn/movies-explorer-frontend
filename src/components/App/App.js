65301518931b4d331c7b69479c622ed11056dcf6
Alexander Filimonov <fil.dsgn@yandex.ru>
2023-07-11 03:13:24 +0300

    dev: add profile jsx

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -32,7 +32,7 @@ function App() {
           />
           <Route path="/profile" element={<Profile />} />
         </Routes>
-        <Footer />
+        {location.pathname !== "/profile" && <Footer />}
       </div>
     </div>
   );
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,67 @@
 import "./Profile.css";
 
+import { useState } from "react";
+
 function Profile() {
-  return;
+  const [isProfileEdit, setIsProfileEdit] = useState(false);
+
+  function toggleProfileButtons() {
+    setIsProfileEdit(!isProfileEdit);
+  }
+
+  return (
+    <section className="profile">
+      <h2 className="profile__title">Привет, Виталий!</h2>
+      <form className="profile__form">
+        <label className="profile__label">
+          Имя
+          <input type="name" value="Виталий" className="profile__input"></input>
+        </label>
+        <label className="profile__label">
+          E-mail
+          <input
+            type="email"
+            value="pochta@yandex.ru"
+            className="profile__input"
+          ></input>
+        </label>
+
+        <button
+          type="button"
+          onClick={toggleProfileButtons}
+          className={`profile__button profile__button_type_edit ${
+            isProfileEdit && "profile__button_hidden"
+          }`}
+        >
+          Редактировать
+        </button>
+        <button
+          className={`profile__button profile__button_type_logout ${
+            isProfileEdit && "profile__button_hidden"
+          }`}
+        >
+          Выйти из аккаунта
+        </button>
+
+        <span
+          className={`profile__error-text ${
+            !isProfileEdit && "profile__error-text_hidden"
+          }`}
+        >
+          При обновлении профиля произошла ошибка.
+        </span>
+        <button
+          type="button"
+          onClick={toggleProfileButtons}
+          className={`profile__button profile__button_type_save ${
+            !isProfileEdit && "profile__button_hidden"
+          }`}
+        >
+          Сохранить
+        </button>
+      </form>
+    </section>
+  );
 }
 
 export default Profile;
