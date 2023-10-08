d0489bbe4508f7706f4fcd5267e4c3824993bb6b
Alexander Filimonov <fil.dsgn@yandex.ru>
2023-10-08 23:01:05 +0300

    fix constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,7 +8,7 @@ export const TABLET_CARDS_AMOUNT = { showCards: 12, addCards: 3 };
 export const TABLET_MINI_CARDS_AMOUNT = { showCards: 8, addCards: 2 };
 export const MOBILE_CARDS_AMOUNT = { showCards: 5, addCards: 2 };
 
-export const DURATION_SHORT_MOVIES = 40;
+export const DURATION_SHORT_MOVIES = 100;
 
 export const PATTERN_EMAIL = "[a-zA-Z0-9_.]+@[a-zA-Z0-9_]+\\.{1,1}[a-z]{2,}";
 
